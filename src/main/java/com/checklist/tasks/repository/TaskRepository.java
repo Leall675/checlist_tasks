1c8b37c9ddd670d1b5379e5a2909e06b9b234821
Matheus Leal Da Silva <maldsilva@rd.com.br>
2025-07-27 22:14:22 -0300

    feat: findByUserId no mongo

--- src/main/java/com/checklist/tasks/repository/TaskRepository.java
+++ src/main/java/com/checklist/tasks/repository/TaskRepository.java
@@ -3,5 +3,9 @@ package com.checklist.tasks.repository;
 import com.checklist.tasks.model.Task;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends MongoRepository<Task, String> {
+    boolean findByTitle(String title);
+    List<Task> findByUserId(String userId);
 }
